1b980600ff566d59727af3b5648efdde0182f56a
Gökhan Aravi <gokhanaravi@gmail.com>
2023-12-19 13:45:58 +0300

    add RunSQLQuery

--- DBDapper/DBDapper.cs
+++ DBDapper/DBDapper.cs
@@ -3,61 +3,74 @@ using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
-public class DBDapper : IDisposable
+namespace DBDapper
 {
-    private readonly string _connectionString;
-    private IDbConnection _connection;
-
-    public DBDapper(string connectionStringName = "SqlConnection")
+    public class DBDapper : IDisposable
     {
-        IConfiguration configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", false, true)
-            .Build();
+        private readonly string? _connectionString;
 
-        _connectionString = configuration.GetConnectionString(connectionStringName);
-    }
-
-    public void Dispose()
-    {
-        _connection?.Close();
-        _connection?.Dispose();
-    }
-
-    private IDbConnection Connection
-    {
-        get
+        public DBDapper(string connectionStringName = "SqlConnection")
         {
-            if (_connection == null)
-            {
-                _connection = new SqlConnection(_connectionString);
-            }
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
 
-            if (_connection.State == ConnectionState.Closed)
-            {
-                _connection.Open();
-            }
-            return _connection;
+            _connectionString = configuration.GetConnectionString(connectionStringName);
         }
-    }
 
-    public string ConnectionString => _connectionString;
+        public void Dispose()
+        {
+            Connection?.Close();
+            Connection?.Dispose();
+            GC.SuppressFinalize(this);
+        }
 
-    public List<T> RunSqlProc<T>(string sp, object parameters = null)
-    {
-        using IDbConnection db = GetOpenConnection();
-        return db.Query<T>(sp, parameters, commandType: CommandType.StoredProcedure).ToList();
-    }
+        private IDbConnection Connection
+        {
+            get
+            {
+                IDbConnection connection = new SqlConnection(_connectionString!);
 
-    public async Task<List<T>> RunSqlProcAsync<T>(string sp, object parameters = null)
-    {
-        using IDbConnection db = GetOpenConnection();
-        return (await db.QueryAsync<T>(sp, parameters, commandType: CommandType.StoredProcedure)).ToList();
-    }
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                return connection;
+            }
+        }
 
-    private SqlConnection GetOpenConnection()
-    {
-        var connection = new SqlConnection(_connectionString);
-        connection.Open();
-        return connection;
+        public string? ConnectionString => _connectionString;
+
+        public List<T> RunSqlProc<T>(string sp, object? parameters = null)
+        {
+            using IDbConnection db = GetOpenConnection();
+            return db.Query<T>(sp, parameters, commandType: CommandType.StoredProcedure).ToList();
+        }
+
+        public async Task<List<T>> RunSqlProcAsync<T>(string sp, object? parameters = null)
+        {
+            using IDbConnection db = GetOpenConnection();
+            return (await db.QueryAsync<T>(sp, parameters, commandType: CommandType.StoredProcedure)).ToList();
+        }
+
+        public List<T> RunSqlQuery<T>(string query, object? parameters = null)
+        {
+            using IDbConnection db = GetOpenConnection();
+            return db.Query<T>(query, parameters, commandType: CommandType.Text).ToList();
+        }
+
+        public async Task<List<T>> RunSqlQueryAsync<T>(string query, object? parameters = null)
+        {
+            using IDbConnection db = GetOpenConnection();
+            var result = await db.QueryAsync<T>(query, parameters, commandType: CommandType.Text);
+            return result.ToList();
+        }
+
+        private SqlConnection GetOpenConnection()
+        {
+            var connection = new SqlConnection(_connectionString!);
+            connection.Open();
+            return connection;
+        }
     }
 }
